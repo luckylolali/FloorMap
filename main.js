b5d48d160d52075c50b4e831cf4cbfb315af6e77
xiaoyan <luckylolali@gmail.com>
2015-02-04 15:30:59 -0500

    add function to show detail

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 /**
  * Created by Elaine on 1/22/15.
  */
+var rooms = {
+    "room301":{
+        "description": "this is room 301",
+        "picture": "http://www.sju.edu/"
+    },
+    "room302":{
+        "description": "this is room 302",
+        "picture": "http://www.sju.edu/"
+    }
+};
 $(document).ready(function(){
     //$("#SVGDoc").attr("data","library_floor_2.svg");
-    loadSvg("library_floor_2.svg");
+    loadSvg("floor_map_test.svg");
     $("#second").click(function(){
         //$("#SVGDoc").attr("data","library_floor_2.svg");
         loadSvg("library_floor_2.svg");
@@ -15,7 +25,9 @@ $(document).ready(function(){
         return false;
     });
 
-
+    $(window).resize(function(){
+        $(".detail").height($("#svgdata").height());
+    });
 });
 
 function loadSvg(filename){
@@ -30,6 +42,17 @@ function loadSvg(filename){
             $(data).find('svg').each(function(){
                 $("#svgdata").html($(this));
             });
+
+        $("[id^=room]").hover(function(){
+            $("#infoDetail").css("display", "block");
+            var room = rooms[this.id];
+            if(room != undefined){
+                $("#roomName").text(this.id);
+                $("#roomDescription").text(room.description);
+            }
+        });
+
+        $(".detail").height($("#svgdata").height());
             //$("#svgdata").html(data);
         });
 }
